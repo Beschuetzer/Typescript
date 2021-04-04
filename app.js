2b7840ba44a6b3785806e443e62040f0ced79ec1
Adam <adam.j.major@gmail.com>
2021-04-04 16:46:31 -0700

    1st version

---
+++ app.js
@@ -0,0 +1,13 @@
+//the output parameter is said to be of type literal (it must be either the string 'number or 'string')
+function combine(input1, input2, output) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number" || output === 'number') {
+        //typescript complains about operations whenever the parameter types are more than one possibility (as above), so we need run-time logic to narrow down which type we're handling where
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+combine('1', '3', 'number');
