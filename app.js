521a1426350e3570286bfc524395ac35169d6aa4
Adam <adam.j.major@gmail.com>
2021-04-06 07:05:55 -0700

    Add shorthand initialization for classes and public vs private examples

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 "use strict";
-var button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', handleClick.bind(null, 'You clicked the button'));
-}
-function handleClick(msg) {
-    console.log(msg);
-}
+var multiply = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (current, next) {
+        return current + next;
+    });
+};
+console.log('multiply(3,1,2,4) =', multiply(3, 1, 2, 4));
+var Employee = (function () {
+    function Employee(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Employee.prototype.getAge = function () {
+        return this.age;
+    };
+    return Employee;
+}());
+var e1 = new Employee('Adam', 29);
+var e2 = new Employee('Andrew', 28);
+var Department = (function () {
+    function Department(name) {
+        this.employees = [];
+        this.name = name;
+    }
+    Department.prototype.describe = function () {
+        console.log('Department' + this.name);
+        console.log("Employees in " + this.name + " =", this.employees.map(function (e) { return e.name + " (" + e.getAge() + ")"; }).join(', '));
+    };
+    Department.prototype.addEmployees = function () {
+        var _a;
+        var employees = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            employees[_i] = arguments[_i];
+        }
+        (_a = this.employees).push.apply(_a, employees);
+    };
+    return Department;
+}());
+var accounting = new Department('accounting');
+accounting.addEmployees(e1, e2);
+accounting.describe();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,9 +1,51 @@
 
-const button = document.querySelector('button');
-if (button) {
-	button.addEventListener('click', handleClick.bind(null, 'You clicked the button'));
+const multiply = (...numbers: number[]) => {
+	return numbers.reduce((current: number, next: number) => {
+		return current + next;
+	})
 }
 
-function handleClick(msg: string) {
-	console.log(msg);
+console.log('multiply(3,1,2,4) =', multiply(3,1,2,4));
+
+
+
+class Employee {
+	//Shorthand initialization
+	constructor(public name: string, private age: number) {}
+
+	//Getter for age as it is private
+	getAge () {
+		return this.age;
+	}
 }
+
+const e1 = new Employee('Adam', 29);
+const e2 = new Employee('Andrew', 28);
+
+class Department {
+	//public is the default
+	public name: string;
+
+	//private properties denote only accessible from inside (this is a ts thing as JS not aware of private props)
+	private employees: Employee[] = [];
+
+	constructor(name: string) {
+		this.name = name;
+	}
+
+	//typescript allows you to provide hints as to what 'this' refers to in a method adding type safety when calling
+	describe(this: Department) {
+		console.log('Department' + this.name);
+		console.log(`Employees in ${this.name} =`, this.employees.map(e => `${e.name} (${e.getAge()})`).join(', '));
+	}
+
+	addEmployees(...employees: Employee[]) {
+		this.employees.push(...employees);
+	}
+}
+
+const accounting = new Department('accounting');
+accounting.addEmployees(e1, e2);
+accounting.describe();
+
+
