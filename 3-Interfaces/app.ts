b261ea6e06a49038b32d98ab6ceda986f8a7b31e
Adam <adam.j.major@gmail.com>
2021-04-06 16:49:04 -0700

    finish interface unit

--- 3-Interfaces/app.ts
+++ 3-Interfaces/app.ts
@@ -39,8 +39,17 @@ user2 = {
 
 //interfaces are usable in classes whereas custom types are not:
 //interfaces force structure similar to how abstract classes do but contain no implementation details
-interface Greetable {
-	name: string;
+
+interface Named {
+	readonly name: string;
+
+	//the '?' means it is optional (works with all method  and function parameters too)
+	outputName?: string;
+}
+
+//interfaces can extend other interfaces (not limited to just one), thereby creating a new interface that requires any classes that implements the inheriting interface to implement all of the interfaces that the inheriting interface extends
+interface Greetable extends Named {
+	//readonly is the only modifier you can use on an interface
 	greet(phrase: string): void;
 }
 
@@ -57,3 +66,24 @@ class Person implements Greetable, IsHuman {
 		console.log(msg);
 	}
 }
+
+//function type
+type AddFn = (a: number, b: number) => number;
+
+//interfaces can by used as function types:
+interface AddFnInterface {
+	(a: number, b: number): number;
+}
+
+let addNumbers: AddFn;
+let addFnInter: AddFnInterface;
+
+addNumbers = (...numbers: number[]) => {
+	return numbers.reduce((last, current) => {
+		return last + current;
+	});
+}
+
+
+//note: interfaces lack a JS analog as they are purely a TS construct
+//note: it is recommended to use interfaces over custom types when describing the structure of objects and functions/methods
