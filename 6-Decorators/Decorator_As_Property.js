a1951873be370d9142ca00de9764a5dff5c415f8
Adam <adam.j.major@gmail.com>
2021-04-10 19:59:23 -0700

    Add decorator that runs on class instantiations

--- 6-Decorators/Decorator_As_Property.js
+++ 6-Decorators/Decorator_As_Property.js
@@ -5,17 +5,48 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function LogPropDecorator(target, propertyName) {
     console.log('property decorator------------------------------------------------');
     console.log('target =', target);
     console.log('propertyName =', propertyName);
 }
-class Product {
+function LogAccessor(target, name, descriptor) {
+    console.log('accessor decorator------------------------------------------------');
+    console.log('target =', target);
+    console.log('name =', name);
+    console.log('descriptor =', descriptor);
+}
+function LogMethod(target, name, descriptor) {
+    console.log('method decorator------------------------------------------------');
+    console.log('target =', target);
+    console.log('name =', name);
+    console.log('descriptor =', descriptor);
+}
+function LogParameter(target, name, position) {
+    console.log('parameter decorator------------------------------------------------');
+    console.log('target =', target);
+    console.log('name =', name);
+    console.log('position =', position);
+}
+function WithTemplateExecutesOnInstantiation(template, hookId) {
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(...args) {
+                super(...args);
+                setTimeout(() => {
+                    console.log(Math.random());
+                }, 10000 * Math.random());
+            }
+        };
+    };
+}
+let Product = class Product {
     constructor(title, _price) {
-        this._price = _price;
         this.title = title;
-    }
-    set title(val) {
+        this._price = _price;
     }
     set price(val) {
         if (val > 0)
@@ -27,10 +58,22 @@ class Product {
             throw Error('Invalid Tax Rate!');
         return this._price * (1 + tax);
     }
-}
+};
 __decorate([
-    LogPropDecorator
-], Product.prototype, "title", null);
+    LogPropDecorator,
+    LogAccessor
+], Product.prototype, "price", null);
+__decorate([
+    LogMethod,
+    __param(0, LogParameter)
+], Product.prototype, "getPriceWithTax", null);
+Product = __decorate([
+    WithTemplateExecutesOnInstantiation('test', 'test')
+], Product);
 const p5 = new Product('apple', 5.55);
+const p6 = new Product('orange', 7.55);
+const p7 = new Product('avocados', 9.55);
 console.log('p5 =', p5);
+console.log('p6 =', p6);
+console.log('p7 =', p7);
 //# sourceMappingURL=Decorator_As_Property.js.map
--- 6-Decorators/Decorator_As_Property.ts
+++ 6-Decorators/Decorator_As_Property.ts
@@ -9,24 +9,65 @@ function LogPropDecorator(target: any, propertyName: string | Symbol) {
   console.log('propertyName =', propertyName);
 }
 
+function LogAccessor(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('accessor decorator------------------------------------------------');
+  console.log('target =', target);
+  console.log('name =', name);
+  console.log('descriptor =', descriptor);
+}
+
+function LogMethod(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('method decorator------------------------------------------------');
+  console.log('target =', target);
+  console.log('name =', name);
+  console.log('descriptor =', descriptor);
+}
+
+function LogParameter(target: any, name: string | Symbol, position: number) {
+  console.log('parameter decorator------------------------------------------------');
+  console.log('target =', target);
+  console.log('name =', name);
+  console.log('position =', position);
+}
+
+//ALL DECORATORS EXECUTE WHEN THE CLASS IS DEFINED NOT WHEN INSTANCES ARE INSTANTIATED UNLESS YOU RETURN A NEW CLASS IN THE DECORATOR:
+
+function WithTemplateExecutesOnInstantiation (template: string, hookId: string) {
+  return function<T extends {new (...args: any[]): {}}> (originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor (...args: any[]) {
+        super(...args);
+        //custom logic to run on each instantiation of the  class the deorator is used on
+        setTimeout(() => {
+          console.log(Math.random())
+        }, 10000 * Math.random())
+      }
+    }
+  }
+}
+
+@WithTemplateExecutesOnInstantiation('test', 'test')
 class Product {
   @LogPropDecorator
-  set title(val: string) {
-    // this.title = val;
-  }
+
+  @LogAccessor
   set price(val: number) {
     if (val > 0) this._price = val;
   }
 
-  constructor(title: string, private _price: number){
-    this.title = title;
+  constructor(public title: string, private _price: number){
   };
 
-  getPriceWithTax(tax: number) {
+  @LogMethod
+  getPriceWithTax(@LogParameter tax: number) {
     if (tax < 0 || tax > 1) throw Error('Invalid Tax Rate!');
     return this._price * (1 + tax);
   }
 }
 
 const p5 = new Product('apple', 5.55);
+const p6 = new Product('orange', 7.55);
+const p7 = new Product('avocados', 9.55);
 console.log('p5 =', p5);
+console.log('p6 =', p6);
+console.log('p7 =', p7);
